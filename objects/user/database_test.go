105c64fcfb2e29540843023e168e2936fcaf5818
Iliushin Valentin <gandarfgamer200219@gmail.com>
2023-05-18 13:51:09 +0300

    Add more user unit test
    Refers to: #11
    
    Signed-off-by: Iliushin Valentin <gandarfgamer200219@gmail.com>

--- objects/user/database_test.go
+++ objects/user/database_test.go
@@ -10,9 +10,29 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func createUser(s *database.Session, login string) error {
+	add_req := user.CreateUserRequest{
+		Login:    login,
+		Password: "qwerty",
+		Role:     1,
+	}
+	return user.AddUser(s, add_req)
+}
+
+func createAdmin(s *database.Session, login string) error {
+	add_req := user.CreateUserRequest{
+		Login:    login,
+		Password: "12345",
+		Role:     4,
+		Keyword:  "kek",
+	}
+	return user.AddUser(s, add_req)
+}
+
 func TestAddGetUser(t *testing.T) {
 	test_support.PrepareForTests()
 	s, err := database.GetReadWriteSession()
+	defer s.Close()
 	require.NoError(t, err)
 
 	user_login := "test_login"
@@ -25,13 +45,7 @@ func TestAddGetUser(t *testing.T) {
 	require.NoError(t, err)
 	require.False(t, exists)
 
-	add_req := user.CreateUserRequest{
-		Login:    user_login,
-		Password: "qwerty",
-		Role:     1,
-	}
-
-	err = user.AddUser(s, add_req)
+	err = createUser(s, user_login)
 	require.NoError(t, err)
 
 	usr, err, exists := user.GetUserById(s, 1)
@@ -49,4 +63,76 @@ func TestAddGetUser(t *testing.T) {
 	require.Equal(t, usr.Id, 1)
 	require.Equal(t, usr.Login, user_login)
 	require.Equal(t, usr.Role, role.Patient)
+}
+
+func TestDoesUserExist(t *testing.T) {
+	test_support.PrepareForTests()
+	s, err := database.GetReadWriteSession()
+	defer s.Close()
+	require.NoError(t, err)
+
+	login1 := "test_login"
+	login2 := "login_test"
+
+	exist, err := user.DoesUserExist(s, login1)
+	require.NoError(t, err)
+	require.False(t, exist)
+
+	err = createUser(s, login1)
+	require.NoError(t, err)
+
+	exist, err = user.DoesUserExist(s, login1)
+	require.NoError(t, err)
+	require.True(t, exist)
+	exist, err = user.DoesUserExist(s, login2)
+	require.NoError(t, err)
+	require.False(t, exist)
+	exist, err = user.DoesUserWithUidExist(s, 1)
+	require.NoError(t, err)
+	require.True(t, exist)
+	exist, err = user.DoesUserWithUidExist(s, 2)
+	require.NoError(t, err)
+	require.False(t, exist)
+
+	err = createUser(s, login2)
+	require.NoError(t, err)
+
+	exist, err = user.DoesUserExist(s, login2)
+	require.NoError(t, err)
+	require.True(t, exist)
+	exist, err = user.DoesUserWithUidExist(s, 2)
+	require.NoError(t, err)
+	require.True(t, exist)
+}
+
+func TestCheckUserRole(t *testing.T) {
+	test_support.PrepareForTests()
+	s, err := database.GetReadWriteSession()
+	defer s.Close()
+	require.NoError(t, err)
+
+	err = createUser(s, "user_login")
+	require.NoError(t, err)
+
+	eq, err, ex := user.CheckUserRole(s, 1, role.Patient)
+	require.NoError(t, err)
+	require.True(t, ex)
+	require.True(t, eq)
+
+	eq, err, ex = user.CheckUserRole(s, 1, role.Doctor)
+	require.NoError(t, err)
+	require.True(t, ex)
+	require.False(t, eq)
+
+	eq, err, ex = user.CheckUserRole(s, 2, role.Admin)
+	require.NoError(t, err)
+	require.False(t, ex)
+
+	err = createAdmin(s, "admin")
+	require.NoError(t, err)
+
+	eq, err, ex = user.CheckUserRole(s, 2, role.Admin)
+	require.NoError(t, err)
+	require.True(t, ex)
+	require.True(t, eq)
 }
--- utils/test_support/test_support.go
+++ utils/test_support/test_support.go
@@ -3,9 +3,11 @@ package test_support
 import (
 	"github.com/GandarfHSE/dentistryBackend/utils/config"
 	"github.com/GandarfHSE/dentistryBackend/utils/migration"
+	"github.com/rs/zerolog"
 )
 
 func PrepareForTests() {
+	zerolog.SetGlobalLevel(zerolog.WarnLevel) // temporary WA, see #42
 	config.LoadTestConfig()
 	migration.MakeHardMigration()
 }
